58dfa8a2ad6efdd50a52c7e30fce34d73722791d
Cedian03 <nyaalgotjohansson@gmail.com>
2025-05-14 16:55:29 +0200

    Oxidize

--- README.md
+++ README.md
@@ -1,12 +1,1 @@
 # cedian.dev
-
-Personal site; live at [cedian.dev](https://cedian.dev).
-
-## Usage
-
-Build and run docker container.
-
-```sh
-docker build -t cedian-dev .
-docker run -d -p 5000:5000 cedian-dev
-```
---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use dioxus::prelude::*;
+
+const FAVICON: Asset = asset!("/assets/favicon.ico");
+const MAIN_CSS: Asset = asset!("/assets/main.css");
+
+fn main() {
+    dioxus::launch(App);
+}
+
+#[component]
+fn App() -> Element {
+    rsx! {
+        document::Link { rel: "icon", href: FAVICON }
+        document::Link { rel: "stylesheet", href: MAIN_CSS }
+        Hero {}
+    }
+}
+
+#[component]
+pub fn Hero() -> Element {
+    rsx! {
+        div {
+            id: "container",
+
+            div {
+                h1 { "cedian.dev" }
+                div {
+                    id: "links",
+
+                    a { href: "https://github.com/Cedian03", "github" }
+                    a { href: "https://www.linkedin.com/in/algot-johansson-9434a22b9/", "linkedin" }
+                }
+            }
+        }
+    }
+}
